169e64434ac0ac5abef3f3841fbc48ce3265856d
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-09-22 22:59:13 -0300

    fix: fixing little issues

--- Módulo 3 - Iniciando na programação com Python (Lógica de programação básica)/Exercício de programação - Cálculo do IMC (índice de Massa Corpórea) + Ellipsis/app.py
+++ Módulo 3 - Iniciando na programação com Python (Lógica de programação básica)/Exercício de programação - Cálculo do IMC (índice de Massa Corpórea) + Ellipsis/app.py
@@ -3,6 +3,6 @@ altura = 1.71
 peso = 97
 imc = peso / altura ** 2
 
-print("Ian Rodrigues tem", altura, "de altura, pesa", peso, "quilos e sei IMC é", imc)
+print(nome, "tem", altura, "de altura, pesa", peso, "quilos e seu IMC é", imc)
 
 # Ian Rodrigues tem 1.71 de altura, pesa 97 quilos e seu IMC é 33.17
